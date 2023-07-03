664fdc159518873a69f53093c6c5a75c0a565fb6
Matthew <16mdlewis@gmail.com>
2023-07-03 19:53:39 +0200

    Added basic project layout

---
+++ Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp;
+
+public class Program
+{
+  /// <summary>
+  /// The main console application entry point
+  /// </summary>
+  public static async Task Main(string[] args)
+  {
+    // Set the synchronization context to null to disable context capturing
+    SynchronizationContext.SetSynchronizationContext(null);
+
+    // Create DI Container
+    using var host = Host.CreateDefaultBuilder(args)
+      .ConfigureServices(services =>
+      {
+        // Register Domain Services
+        // TODO:
+
+        // Register Application Services
+        // TODO:
+      })
+      .Build();
+
+    // Call the delegator service
+    // TODO: add service here (var service = host.Services.GetRequiredService<IService>())
+    // TODO: use service
+  }
+}
