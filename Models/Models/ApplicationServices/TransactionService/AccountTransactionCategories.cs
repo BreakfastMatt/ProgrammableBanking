3f0c8157d8ab2b444e396f71e8e8aee2c012a087
Matthew <16mdlewis@gmail.com>
2023-07-04 22:21:43 +0200

    Cleanup

--- Models/Models/ApplicationServices/TransactionService/AccountTransactionCategories.cs
+++ Models/Models/ApplicationServices/TransactionService/AccountTransactionCategories.cs
@@ -1,7 +1,9 @@
 ﻿namespace Models.Models.ApplicationServices.TransactionService;
 
+/// <summary>
+/// The transaction categories as configured in the config.json file
+/// </summary>
 public class AccountTransactionCategories
 {
   public Dictionary<string, List<string>> AccountTransactions { get; set; }
-  //public List<string> AccountTransactions { get; set; }
 }
